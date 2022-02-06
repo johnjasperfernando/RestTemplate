4b15c4a5fd01fbb21df6a610c034f1292f2ce778
johnjasperfernando <john.jasper2107@gmail.com>
2022-02-06 22:24:40 +0530

    organized the code

--- src/main/java/com/restTemplateExample/RestTemplate/Appconfig/DataSourceConfig.java
+++ src/main/java/com/restTemplateExample/RestTemplate/Appconfig/DataSourceConfig.java
@@ -27,7 +27,6 @@ public class DataSourceConfig {
     @Bean(name="jdbcTemplate")
     public JdbcTemplate jdbcTemplate(@Qualifier("db")DataSource jdbcdataSource)
     {
-        return jdbcTemplate((DataSource) jdbcdataSource);
         return new JdbcTemplate(((DataSource) jdbcdataSource));
     }
 
